4fc731f75c58e66c8840f88d886419d375358723
Brendan O'Connor <sp.brendan@gmail.com>
2016-01-19 21:44:33 +0300

    Removing steering, using waypoints with NavMeshAgent

--- Assets/_Scripts/Citizen.cs
+++ Assets/_Scripts/Citizen.cs
@@ -2,12 +2,16 @@
 
 public class Citizen : MonoBehaviour
 {
+    public Transform[] pointsOfInterest;
+
+    private int currentInterest = 0;
     private GameObject target;
     private NavMeshAgent agent;
 
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        GoToPointOfInterest();
     }
 
     void Update()
@@ -15,6 +19,10 @@ public class Citizen : MonoBehaviour
         if (target)
         {
             agent.destination = target.transform.position;
+        }
+        else
+        {
+            GoToPointOfInterest();
         }
     }
 
@@ -24,6 +32,26 @@ public class Citizen : MonoBehaviour
         if (other.gameObject.tag == "Player")
         {
             this.target = other.gameObject;
+            agent.autoBraking = true;
         }
+    }
+
+    void GoToPointOfInterest()
+    {
+        // check if there's anywhere to go
+        if (pointsOfInterest.Length == 0)
+        {
+            return;
+        }
+
+        if (agent.remainingDistance < 0.5f || agent.destination == Vector3.zero)
+        {
+            currentInterest = (currentInterest + 1) % pointsOfInterest.Length;
+            Debug.Log(currentInterest);
+            agent.destination = pointsOfInterest[currentInterest].position;
+        }
+
+        agent.autoBraking = false;
+
     }
 }
