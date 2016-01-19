a43d53928fd520b446c83660e7382f2107d6b51e
Brendan O'Connor <sp.brendan@gmail.com>
2016-01-19 23:42:41 +0300

    Basic spawn script and prefabs

--- Assets/_Scripts/GameController.cs
+++ Assets/_Scripts/GameController.cs
@@ -1,12 +1,64 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GameController : MonoBehaviour {
+public class GameController : MonoBehaviour
+{
+    // Prefabs
+    public GameObject[] zombiePrefabs;
+    public GameObject[] citizenPrefabs;
+    
+    // spawnlocations
+    public Transform[] zombieLocations;
+    public Transform[] citizenLocations;
 
-	// Use this for initialization
-	void Start () {
-	
+    // counters
+    private int rescued = 0;
+    private int zombified = 0;
+
+	void Start ()
+    {
+        // spawn the zombies
+        if (zombiePrefabs.Length > 0)
+        {
+            for(int i = 0; i < zombieLocations.Length; i++) {
+                Instantiate(zombiePrefabs[(i % zombiePrefabs.Length)], zombieLocations[i].position, Quaternion.identity);
+            }
+        }
+
+        // spawn the citizens
+        if (citizenPrefabs.Length > 0)
+        {
+            for (int i = 0; i < citizenLocations.Length; i++) {
+                Instantiate(citizenPrefabs[(i % citizenPrefabs.Length)], citizenLocations[i].position, Quaternion.identity);
+            }
+        }
 	}
+
+    public int Zombified
+    {
+        set
+        {
+            this.zombified = value;
+        }
+
+        get
+        {
+            return this.zombified;
+        }
+    }
+
+    public int Rescued
+    {
+        set
+        {
+            this.rescued = value;
+        }
+
+        get
+        {
+            return this.rescued;
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {
